7131f1cb312594e85b428e4806d7bc0fc88ec516
Muhammad Hamza <muhammadhamza@Muhammads-MacBook-Pro.local>
2025-04-06 12:52:23 +0100

    database connection

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from flask import Flask
+from pymongo import MongoClient
 
 app = Flask(__name__)
 
+# Created Mongodb on mongo atlas and connnected in my app
+client = MongoClient("mongodb+srv://hamzamayo6412:pXe8HIv6Lx5QhoqI@cluster0.bc8avdt.mongodb.net/", tls=True, tlsAllowInvalidCertificates=True)
+db = client['Econ_store']  # Your database name
+collection = db['products']  # Your collection name
 
 @app.route('/')
 def index():
